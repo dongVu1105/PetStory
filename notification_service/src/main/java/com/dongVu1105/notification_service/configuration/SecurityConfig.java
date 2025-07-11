7de37793f3271a6d6dc4f1ce4b302894ac1dd32d
Vu Huy Dong <huydongvu0105@gmail.com>
2025-07-11 20:35:25 +0700

    initial post-service

--- notification_service/src/main/java/com/dongVu1105/notification_service/configuration/SecurityConfig.java
+++ notification_service/src/main/java/com/dongVu1105/notification_service/configuration/SecurityConfig.java
@@ -19,7 +19,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
     private static final String[] PUBLIC_ENDPOINTS = {
-            "/email/send"
     };
 
     private final CustomJwtDecoder customJwtDecoder;
